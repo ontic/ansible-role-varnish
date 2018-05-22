3e8d3bb0a001975e3a20cacf5ac6565bfd30e113
Adam Dyson <adam.dyson@outlook.com>
2018-05-23 04:11:55 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
-# Verify the installed version of Varnish.
+# Verify the installed location.
+docker exec --tty ${container_id} env TERM=xterm which varnishd
+# Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm varnishd -V
-
 # Verify Varnish is accessible through port 80.
 docker exec --tty ${container_id} env TERM=xterm curl -sI localhost:80 | grep -q "Via: .* varnish"
